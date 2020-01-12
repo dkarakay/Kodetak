af8f63cdf4e65e99e108e7527d52ca2a866f1ca8
ozgurkara99 <49339814+ozgurkara99@users.noreply.github.com>
2020-01-12 20:38:49 +0300

    Update flask_web.py

--- flask_web.py
+++ flask_web.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 
-a = 5;
 @app.route('/', methods=['GET'])
 def hello():
 
@@ -13,7 +11,4 @@ def hello():
     return render_template('home.html',something=incorrect_answers[-1])
 
 if __name__ == '__main__':
-    #scheduler = APScheduler()
-    #scheduler.add_job(func=hello,args=['job run'],trigger='interval',id='job', seconds=5)
-    #scheduler.start()
     app.run(debug=True,host='0.0.0.0',port = 5050,threaded=True)
